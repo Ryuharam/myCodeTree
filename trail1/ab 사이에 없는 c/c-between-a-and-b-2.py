af696be6044ae0a9f61256fdb10a1332a75be596
haram <dlfnadl97@naver.com>
2026-05-26 15:09:04 +0900

    [Passed] ab 사이에 없는 c (56ms, 16MB)

---
+++ trail1/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,17 @@
+a,b,c = input().split()
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+flag = False
+
+for i in range(a, b+1):
+    if i%c==0:
+        flag = True
+        break
+
+if not flag:
+    print("YES")
+else:
+    print("NO")
