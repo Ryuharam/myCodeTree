895a2449c4dd12ae7a6e431407ebd94a061d739c
haram <dlfnadl97@naver.com>
2026-06-03 22:24:46 +0900

    [Passed] 함수를 이용한 소수 판별 (48ms, 16MB)

---
+++ trail2/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,20 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+
+issosu = bool()
+
+def issosu(n:int):
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    
+    return True
+
+sum = 0
+
+for i in range(a,b+1):
+    if issosu(i):
+        sum += i
+
+print(sum)
