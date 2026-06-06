4dbcc1c8f36e1f0e36b5b3732564b7fdc715c5bc
haram <dlfnadl97@naver.com>
2026-06-06 17:11:59 +0900

    [Passed] 1이 되는 순간까지 (51ms, 16MB)

---
+++ trail2/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+# Please write your code here.
+
+def cal(n):
+    if n <= 1:
+        return 0
+    if n%2==0:
+        return cal(n//2) + 1
+    else:
+        return cal(n//3) + 1
+
+print(cal(N))
