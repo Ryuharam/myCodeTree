706504435387d1051f01b0e1e83deaf8fb30c992
haram <dlfnadl97@naver.com>
2026-06-10 09:42:02 +0900

    [Passed] Time to Time (50ms, 16MB)

---
+++ trail2/Time to Time/time-to-time.py
@@ -0,0 +1,15 @@
+a, b, c, d = map(int, input().split())
+ans = 0
+
+# Please write your code here.
+while True:
+    if a==c and b==d:
+        break
+    b += 1
+    ans += 1
+
+    if b==60:
+        a += 1
+        b = 0
+
+print(ans)
