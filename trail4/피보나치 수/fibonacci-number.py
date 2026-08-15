1b95a1c192a671a7ad46d416d478ef12d76ffd6a
haram <dlfnadl97@naver.com>
2026-08-15 14:24:37 +0900

    [Passed] 피보나치 수 (47ms, 16MB)

--- trail4/피보나치 수/fibonacci-number.py
+++ trail4/피보나치 수/fibonacci-number.py
@@ -1,15 +1,15 @@
+import sys
+
 N = int(input())
 
-memo = [-1 for _ in range(N+1)]
+dp = [0 for _ in range(N+1)]
+if N == 1 or N == 2:
+    print(1)
+    sys.exit()
+dp[1] = 1
+dp[2] = 1
 
-def memoization(n: int):
-    if memo[n] != -1:
-        return memo[n]
+for i in range(3, N+1):
+    dp[i] = dp[i-1] + dp[i-2]
 
-    if n <= 2:
-        return 1
-
-    memo[n] = memoization(n-2) + memoization(n-1)
-    return memo[n]
-
-print(memoization(N))
+print(dp[N])
